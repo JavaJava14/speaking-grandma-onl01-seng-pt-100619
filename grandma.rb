05dfd622901edad850ec10582f2f685359c2e28a
Jose Chavira <joeychavira2016@gmail.com>
2019-10-09 06:05:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 def speak_to_grandma(phrase)
   if phrase = "i love you grandma!".upcase
     return "i love you too pumpkin!".upcase
-  elsif phrase != phrase.upcase
+  if phrase != phrase.upcase
      return "huh?! speak up, sonny!".upcase
   else
     return  "no, not since 1938!".upcase
